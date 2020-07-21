e732c350504a34b80d7c0b7a62a9fde8d4059739
Prince abdurrahman <abil7530@gmail.com>
2020-07-21 09:30:42 +0700

    Belajar View Binding

---
+++ app/src/main/java/com/princeabdurrahman/viewbinding/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.princeabdurrahman.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
+import com.princeabdurrahman.viewbinding.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.btnSend.setOnClickListener {
+            val data = edit_text.text
+            saveData(data.toString())
+        }
+    }
+
+    private fun saveData(data: String) {
+        binding.textView.text = data
+
+    }
+}
